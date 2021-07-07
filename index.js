715e6a112f7acf8206addc379e9031fc2a12706c
NatanaelHZ <natanael_henrik28@outlook.com>
2021-07-06 22:28:38 -0300

    Adicionado e configurado sequelize e criado model user

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const routes = require('./src/routes');
+const db = require("./src/models");
 
 const app = express();
 
 app.use(routes);
 
+db.sequelize.sync();
+
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening at http://localhost:${process.env.PORT}`);
 });
+
+//Código exemplo model
+//https://bezkoder.com/node-express-sequelize-postgresql/#Initialize_Sequelize
---
+++ src/models/index.js
@@ -0,0 +1,13 @@
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres'
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.user = require("./user.model.js")(sequelize, Sequelize);
+
+module.exports = db;
