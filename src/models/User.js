06ef389ffb57b1144c5590087d502f2a1593d08c
NatanaelHZ <natanael_henrik28@outlook.com>
2021-08-17 22:51:42 -0300

    Changed field configuration

--- src/models/User.js
+++ src/models/User.js
@@ -5,13 +5,17 @@ const SALT_ROUNDS = 6;
 module.exports = (sequelize, Sequelize) => {
   const User = sequelize.define('user', {
     name: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING,
+      allowNull: false
     },
     email: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
     },
     password: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING,
+      allowNull: false
     }
   },{
     hooks: {
@@ -19,7 +23,13 @@ module.exports = (sequelize, Sequelize) => {
         const hash = bcrypt.hashSync(user.password, SALT_ROUNDS);
         user.password = hash;
       }
-    }
+    },
+    indexes: [
+      {
+        unique: true,
+        fields: ['email']
+      }
+    ]
   });
 
   return User;
