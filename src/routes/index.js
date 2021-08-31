b5d52edc687a0a31c14795ad08c3fec53aae7b41
NatanaelHZ <natanael_henrik28@outlook.com>
2021-08-31 20:36:39 -0300

    Integrated vaccine route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const userRoutes = require('./userRoutes');
+const vaccineRoutes = require('./vaccineRoutes');
 
 const routes = Router();
 
 userRoutes(routes);
+vaccineRoutes(routes);
 
 module.exports = routes;
