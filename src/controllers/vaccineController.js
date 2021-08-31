0cf962cdb62d14a5692adc050634aab1faf1cd01
NatanaelHZ <natanael_henrik28@outlook.com>
2021-08-31 20:35:47 -0300

    Create controller vaccine

---
+++ src/controllers/vaccineController.js
@@ -0,0 +1,16 @@
+const { Vaccine } = require('../models');
+
+exports.create = async (req, res) => {
+  try {
+    const vaccine = await Vaccine.create(req.body);
+    
+    return res.status(201).json({
+      vaccine: vaccine,
+      message: 'success_create_vaccine'
+    });
+  } catch (e) {
+    return res.status(400).json({
+      message: e.message
+    });
+  }
+};
