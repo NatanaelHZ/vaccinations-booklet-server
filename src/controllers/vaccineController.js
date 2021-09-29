e67f362898ef33631528f7ee66237ba67ccc9350
NatanaelHZ <natanael_henrik28@outlook.com>
2021-09-28 22:07:48 -0300

    Returning vaccine

--- src/controllers/vaccineController.js
+++ src/controllers/vaccineController.js
@@ -2,9 +2,11 @@ const { Vaccine } = require('../models');
 
 exports.create = async (req, res) => {
   try {
-    const vaccine = await Vaccine.create(req.body);
-    
-    return res.status(201).json({ message: 'success_create_vaccine' });
+    const vaccine = { ...req.body, user_id: req.currentUser.id };
+
+    const vaccineCreated = await Vaccine.create(vaccine);
+
+    return res.status(201).json({ vaccine: vaccineCreated, message: 'success_create_vaccine' });
   } catch (e) {
     return res.status(400).json({
       message: e.message
